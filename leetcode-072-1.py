c43bf5e4e91540723bee85d2a8c7777e5b7ad2c0
Benjamin Huang <benhua476@berkeley.edu>
2025-08-16 19:10:17 -0700

    levelshtein implementation complete

--- leetcode-072-1.py
+++ leetcode-072-1.py
@@ -1,6 +1,20 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        print("hi")
-        
+        input_len = len(word1)
+        target_len = len(word2)
+        steps = [[-1 for _ in range(target_len+1)] for _ in range(input_len+1)]
+        for i in range(0, input_len + 1):
+            steps[i][target_len] = input_len - i
+        for i in range(0, target_len + 1):
+            steps[input_len][i] = target_len - i
+
+        def getSteps(row, col):
+            if (steps[row][col] == -1):
+                if (word1[row] == word2[col]):
+                    steps[row][col] = getSteps(row + 1, col + 1);
+                else:
+                    steps[row][col] = 1 + min(getSteps(row + 1, col), getSteps(row, col + 1), getSteps(row + 1, col + 1))
+            return steps[row][col]
+        return getSteps(0,0)
 solution = Solution()
-print(solution.minDistance("", "a"))
+print(solution.minDistance("dinitrophenylhydrazine", "acetylphenylhydrazine"))

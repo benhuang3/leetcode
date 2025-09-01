7a49a186f6fb70b0a5a812c3163191bc76110dc0
Benjamin Huang <benhua476@berkeley.edu>
2025-09-01 15:45:16 -0700

    better solution without edge case, lc-010

--- lc-010-regexmatch-hard/lc-010-1.py
+++ lc-010-regexmatch-hard/lc-010-1.py
@@ -1,6 +1,8 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
         def matchChar(expr_c, str_c):
+            if str_c == ';':
+                return False
             if expr_c == '.':
                 return True
             else:
@@ -18,37 +20,28 @@ class Solution:
                     expr_i += 1
                 tokens.append(expr_char)
                 expr_i += 1 
-
             return tokens
         def isStar(expr):
             return expr[0] if len(expr) > 1 else None
-        
+        s = s + ';'
         tokens = tokenize()
         dp = [[False for _ in range(len(tokens) + 1)] for _ in range(len(s) + 1)]
         dp[0][0] = True if isStar(tokens[0]) else matchChar(tokens[0], s[0]) 
 
-        for i in range(len(s) + 1):
+        for i in range(len(s)):
             for j in range(len(tokens)):
-                
-                
-                if dp[i][j]:
-                    
-                    #border condition
-                    if i == len(s):
-                        if isStar(tokens[j]):
-                            dp[i][j+1] = True
-                        continue
 
+                if dp[i][j]:
                     str_char = s[i]
                     expr_char = tokens[j]
-                
+
                     if matchChar(expr_char[0], str_char):
                         dp[i+1][j+1] = True
-                    if isStar(expr_char):
+                    if isStar(tokens[j]):
                         dp[i][j+1] = True
-                    if isStar(expr_char) and matchChar(expr_char[0], str_char):
-                        dp[i+1][j] = True
+                        if matchChar(expr_char[0], str_char):
+                            dp[i+1][j] = True
         for row in dp:
             print(row)
-        return dp[len(s)][len(tokens)]
+        return dp[len(s) - 1][len(tokens)]
         

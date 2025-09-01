f3c4cad6e8cecc061e939227031b72e6dfc616c1
Benjamin Huang <benhua476@berkeley.edu>
2025-08-31 19:22:40 -0700

    optimal for two sorted median problem lc-004

--- lc-004-mediantwosorted-hard/lc-004-1.py
+++ lc-004-mediantwosorted-hard/lc-004-1.py
@@ -1,25 +1,28 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        def merge(l1, l2):
-            result = []
-            i,j = 0, 0
-            while i < len(l1) and j < len(l2):
-                if l1[i] <= l2[j]:
-                    result.append(l1[i])
-                    i += 1
-                else:
-                    result.append(l2[j])
-                    j += 1
-            result = result + l1[i:] + l2[j:]
-            return result
-        
-        merged = merge(nums1, nums2)
-        print(merged)
-        if len(merged) % 2 == 1:
-            return merged[len(merged) // 2]
-        else:
-            return (merged[len(merged) // 2 - 1] + merged[len(merged) // 2]) / 2
+        short_list, long_list = (nums1, nums2) if len(nums1) <= len(nums2) else (nums2, nums1)
 
-    
-solution = Solution()
-print(solution.findMedianSortedArrays([1,2], [3,4]))
+        low = 0
+        high = len(short_list)
+        half_length = (len(short_list) + len(long_list)) // 2
+        while True:
+            i1 = (low + high) // 2
+            i2 = half_length - i1
+            short_cur = -float("inf") if i1 == 0 else short_list[i1 - 1]
+            long_cur = -float("inf") if i2 == 0 else long_list[i2 - 1]
+            short_next = float("inf") if i1 >= len(short_list) else short_list[i1]
+            long_next = float("inf") if i2 >= len(long_list) else long_list[i2]
+
+            if short_cur <= long_next and long_cur <= short_next:
+                break
+            if short_cur > long_next:
+                high = i1 - 1
+                low = low
+            else:
+                high = high
+                low = i1 + 1
+                
+        if (len(nums1) + len(nums2)) % 2 == 1:
+            return min(short_next, long_next)
+        else:
+            return (min(short_next, long_next) + max(short_cur, long_cur)) / 2

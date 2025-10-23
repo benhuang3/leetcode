179e24b25b214141a3675c60c227f7f113b86541
Benjamin Huang <benhua476@berkeley.edu>
2025-10-22 21:40:08 -0700

    finished lc 009

---
+++ lc-009-palindromenum-easy/lc-009-0.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        xs = str(x)
+        xs = xs[::-1]
+        for i in range(0, len(xs)):
+            if xs[i] != xs[len(xs) - 1 - i]:
+                return False
+
+        return True
+        

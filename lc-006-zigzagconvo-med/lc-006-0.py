be96755020d9a627becf633abdbd4f94c149d2c4
Benjamin Huang <benhua476@berkeley.edu>
2025-10-23 19:17:10 -0700

    finished problem with mathy solution

---
+++ lc-006-zigzagconvo-med/lc-006-0.py
@@ -0,0 +1,31 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1:
+            return s
+        if numRows >= len(s):
+            return s
+        res = ""
+        
+        for cur_row in range(0, numRows):
+            cur_index = cur_row
+            on_diagonal = False
+
+            res = res + s[cur_index]
+
+            while True:
+                
+                if on_diagonal:
+                    to_next_index = 2 * cur_row
+
+                elif not on_diagonal:
+                    to_next_index = 2 * (numRows - 1 - cur_row)
+                
+                cur_index += to_next_index
+
+                if cur_index >= len(s):
+                    break
+
+                if to_next_index != 0:
+                    res = res + s[cur_index]
+                on_diagonal = not on_diagonal
+        return res
